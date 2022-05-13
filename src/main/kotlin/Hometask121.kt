6582803940cb301be0091636d9aa32481037def4
Евгений <eugeny1988@gmail.com>
2022-05-13 22:33:35 +0300

    Init commit

---
+++ src/main/kotlin/Hometask121.kt
@@ -0,0 +1,10 @@
+val commission = 0.0075
+val min_commission = 35.00
+fun main(){
+    print("Введите сумму перевода: ")
+    var amount = readln().toDouble()
+    var score_commission = if (amount*commission<=min_commission) min_commission
+    else amount*commission
+    print("Размер комиссии составит: $score_commission")
+
+}
