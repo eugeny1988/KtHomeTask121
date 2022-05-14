aa79390750ab1d571d31f5c78e06acc459cc1c0d
Евгений <eugeny1988@gmail.com>
2022-05-14 23:39:41 +0300

    Reformat code

--- src/main/kotlin/Hometask121.kt
+++ src/main/kotlin/Hometask121.kt
@@ -1,10 +1,10 @@
 val commission = 0.0075
-val min_commission = 35.00
-fun main(){
+val min_commission = 3500
+fun main() {
     print("Введите сумму перевода: ")
     var amount = readln().toDouble()
-    var score_commission = if (amount*commission<=min_commission) min_commission
-    else amount*commission
+    var score_commission = if (amount * commission <= min_commission) min_commission
+    else amount * commission
     print("Размер комиссии составит: $score_commission")
 
 }
