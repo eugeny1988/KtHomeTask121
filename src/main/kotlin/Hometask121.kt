16c1718b9b0740fa2f24e9503391ace9cf4cdb30
Евгений <eugeny1988@gmail.com>
2022-05-15 13:16:15 +0300

    Added camelCase

--- src/main/kotlin/Hometask121.kt
+++ src/main/kotlin/Hometask121.kt
@@ -1,10 +1,10 @@
 val commission = 0.0075
-val min_commission = 3500
+val minCommision = 3500
 fun main() {
     print("Введите сумму перевода: ")
     var amount = readln().toDouble()
-    var score_commission = if (amount * commission <= min_commission) min_commission
+    var scoreCommission = if (amount * commission <= minCommision) minCommision
     else amount * commission
-    print("Размер комиссии составит: $score_commission")
+    print("Размер комиссии составит: $scoreCommission")
 
 }
